ecc7d57c0de5f294f1237204bb714044d87602c1
Tom <tomasnaiouf@gmail.com>
2022-02-13 14:22:11 -0300

    Add decimal function

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ function operandCallback(e){
     const field = document.querySelector(".input.field");
     if(field.textContent != "0")field.textContent+=e.target.textContent;
     else field.textContent=e.target.textContent;
-    
+}
+
+function decimalCallBack(e){
+    const field = document.querySelector(".input.field");
+    if(field.textContent.indexOf('.') == -1) field.textContent+="."
 }
 
 
@@ -11,4 +15,9 @@ const operands = document.querySelectorAll(".button.operand");
 operands.forEach(operand=>{
     operand.addEventListener("click",operandCallback)
 })
+
+const decimal = document.querySelector(".button.decimal");;
+decimal.addEventListener("click",decimalCallBack)
+
+
 
